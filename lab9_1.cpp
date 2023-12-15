2ca0846258ea8f117fa418a49b0bd433759ac377
ChawinKatpark <141801767+ChawinKatpark@users.noreply.github.com>
2023-12-15 08:39:04 +0000

    lab9_1.cpp completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
+double loan, rate, can;
+int year = 1, i = 0;
 
 int main(){	
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> can;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +23,43 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+	double realrate = (rate*loan)/100;
+	double total = loan+realrate;
+	double last = total-can;
+	
+	while (last > 0) {
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << realrate;
+    cout << setw(13) << left << total;
+	if (can > total) {
+	    can = total;
+    }
+    cout << setw(13) << left << can;	 
+    cout << setw(13) << left << last;
+    last = total-can;
+    cout << "\n";
+    
+    year = year+1;
+    loan = last;
+    realrate = (rate*loan)/100;
+    total = loan+realrate;
+    last = total-can;
+	}
+	
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << realrate;
+    cout << setw(13) << left << total;
+	if (can > total) {
+	    can = total;
+	    last = total-can;
+    }
+    cout << setw(13) << left << can;	 
+    cout << setw(13) << left << last;
+    cout << "\n";
 	
 	return 0;
 }
